54d571300fff8f3ee04894c3b7b439f0c7cd97df
N V S Abhishek <nvsabhishek.india@gmail.com>
2018-12-17 00:24:50 +0530

    loader added

--- js/main.js
+++ js/main.js
@@ -31,22 +31,26 @@
      
      var ssPreloader = function() {
         
-         $("html").addClass('ss-preload');
+         //$("html").addClass('ss-preload');
 
     $WIN.on('load', function() {
 
-           //force page scroll position to top at page refresh
-          $('html, body').animate({ scrollTop: 0 }, 'normal');
+    //        //force page scroll position to top at page refresh
+    //       $('html, body').animate({ scrollTop: 0 }, 'normal');
 
-          // will first fade out the loading animation 
-    $("#loader").fadeOut("slow", function() {
-           // will fade out the whole DIV that covers the website.
-               $("#preloader").delay(300).fadeOut("slow");
-           }); 
+    //       // will first fade out the loading animation 
+    // $("#loader").fadeOut("slow", function() {
+    //        // will fade out the whole DIV that covers the website.
+    //            $("#preloader").delay(300).fadeOut("slow");
+    //        }); 
             
-          // for hero content animations 
-       $("html").removeClass('ss-preload');
-           $("html").addClass('ss-loaded');
+    //       // for hero content animations 
+    //    $("html").removeClass('ss-preload');
+    //        $("html").addClass('ss-loaded');
+
+    jQuery("#status").fadeOut();
+        // will fade out the whole DIV that covers the website.
+    jQuery("#preloader").delay(1000).fadeOut("slow");
         
       });
      };
